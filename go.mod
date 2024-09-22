b78f2ded51f0f5d70f3541687867f0f19d42adc9
Pablo Calvo <pjcalvov@gmail.com>
2024-09-22 09:03:00 +0000

    first commit to dev, running server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pjcalvo/homa-webservice
+
+go 1.23.0
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+type cmdresult struct {
+	Success bool   `json:"success`
+	Message string `json:"message`
+}
+
+func homepage(writer http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(writer, "Go Home Simple Rest Api Server")
+}
+func getdate(writer http.ResponseWriter, _ *http.Request) {
+	var result cmdresult
+
+	out, err := exec.Command("date").Output()
+	if err == nil {
+		result.Success = true
+		result.Message = fmt.Sprintf("The date is %s", string(out))
+	} else {
+		result.Message = fmt.Sprintf("Errored out %v", err)
+	}
+
+	json.NewEncoder(writer).Encode(result)
+}
+
+func main() {
+	http.HandleFunc("/", homepage)
+	http.HandleFunc("/api/v1/getdate", getdate)
+	err := http.ListenAndServe(":4000", nil)
+	if err != nil {
+		fmt.Printf("Failed to start server: %v\n", err)
+		os.Exit(1)
+	}
+}
